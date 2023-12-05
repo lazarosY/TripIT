aefdca5746ffb191c10e73e61ec83c4e52230775
lazarosY <148948579+lazarosY@users.noreply.github.com>
2023-12-05 13:41:10 +0200

    Fixed the "\n" showing in .txt bug

--- Desktop/TRIPI_T/ProgramCreator.java
+++ Desktop/TRIPI_T/ProgramCreator.java
@@ -1,5 +1,8 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
 import java.io.DataOutputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.File;
 public class ProgramCreator {
@@ -15,8 +18,8 @@ public class ProgramCreator {
 }
     public static void programMaker(String gptresponse) throws IOException {
         File file =  new File(System.getProperty("user.home") + "/" +"vacation program.txt");
-        try (var out = new DataOutputStream(new FileOutputStream(file))) {
-            out.writeUTF(gptresponse);
+        try (var out = new BufferedWriter(new FileWriter(file))) {
+            out.write(gptresponse);
         } catch (IOException e) {
             System.err.println("Couldn't write byte" + e.getMessage());
             System.exit(1);
