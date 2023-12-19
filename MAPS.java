780810d5ddb366cce8505fc2def779643f33f49c
lazarosY <148948579+lazarosY@users.noreply.github.com>
2023-12-19 20:35:02 +0200

    Made some formational changes to the response string

--- MAPS.java
+++ MAPS.java
@@ -5,14 +5,58 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import org.json.JSONArray;
+import org.json.JSONObject;
 public class MAPS{
     public static void main (String[] args){
         String dest= "athens greece";
         String place_id="ChIJgSMyRUu9oRQRS9UFR1xmJrE";
         MAPS help = new MAPS();
-       System.out.print(help.GoogleMaps(place_id));
+        JSONObject json = new JSONObject(help.GoogleReviews(place_id));
+        JSONArray jsonarr = json.getJSONObject("result").getJSONArray("reviews");
+        for (int i=0;i<jsonarr.length();i++){
+            JSONObject review = jsonarr.getJSONObject(i);
+            String sreview = review.toString();
+            System.out.println(sreview);
+        }
+       
+        System.out.print("Overall"+help.GoogleRating(place_id));
     }
-    public static String GoogleMaps(String placeid) {
+    
+    
+    public static String GoogleRating(String placeid) {
+    //HTTP REQUEST for the API
+    String gapiKey = "AIzaSyBz32QZHuvccsYTCsAe9Wxd0RCWO2hXDas";
+    String gurl=("https://maps.googleapis.com/maps/api/place/details/json?fields=rating&place_id="+placeid+"&key="+gapiKey);//instead of reviews I can add rating, for a general rating
+    try{ 
+        // HTTP REQUEST
+        URL gmaps = new URL(gurl);
+        HttpURLConnection mapscon = (HttpURLConnection) gmaps.openConnection();
+        mapscon.setRequestMethod("GET");
+        // HTTP REQUEST
+        
+        //HTTP RESPONSE
+        BufferedReader mapsin = new BufferedReader(new InputStreamReader(mapscon.getInputStream()));
+        String mapsinputline;
+        StringBuffer mresponse = new StringBuffer();
+        while((mapsinputline=mapsin.readLine()) != null) {
+            mresponse.append(mapsinputline);
+        }
+        mapsin.close();
+        String resp = mresponse.toString();
+        resp = GetSubstring(resp);
+        return resp;
+        
+    }catch(IOException E3){
+        throw new RuntimeException(E3);
+    }
+}
+    
+    
+    
+    
+    
+    public static String GoogleReviews(String placeid) {
     //HTTP REQUEST for the API
     String gapiKey = "AIzaSyBz32QZHuvccsYTCsAe9Wxd0RCWO2hXDas";
     String gurl=("https://maps.googleapis.com/maps/api/place/details/json?fields=reviews&place_id="+placeid+"&key="+gapiKey);//instead of reviews I can add rating, for a general rating
@@ -31,9 +75,20 @@ public class MAPS{
             mresponse.append(mapsinputline);
         }
         mapsin.close();
-        return mresponse.toString();
+        String resp = mresponse.toString();
+        return resp;
+    
     }catch(IOException E2){
         throw new RuntimeException(E2);
     }
 }
+
+public static String GetSubstring(String response) {
+    int startMarker = response.indexOf("rating")-1; // Marks the start of the response
+    int endMarker = response.indexOf("}", startMarker); // Marks the end of the response
+    return response.substring(startMarker, endMarker); // Returns the response string
+}
+
+
+
 }
